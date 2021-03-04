011a6ba10f43fa8dbab02a036bfc99abf01b4a7d
s-oganisyan <s.oganisyan.exceedteam@gmail.com>
2021-03-04 16:47:30 +0300

    a mechanism for separating stripe events has been created

--- api/index.js
+++ api/index.js
@@ -2,9 +2,12 @@ require("../models/index");
 const express = require("express");
 
 const stripeWebhooks = require("./routes/stripeWebhooks");
+const healthcheck = require("./routes/healthcheck");
 
 const router = express.Router();
 
 router.use("/", stripeWebhooks);
+router.use("/", healthcheck);
+
 
 module.exports = router;
